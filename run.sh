022941631d87d931fd539a0979ba2bd9d8276bcb
lutbook <lutbuk@gmail.com>
2020-09-01 16:15:01 +0900

    run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python setup.py build develop
+cd
+cd
+run python3 train.py --config=yolact_plus_custom_config --resume=/kw_resources/yolact_data/weights/yolact_resnet101_line_tree_266_20000.pth --start_iter=20001 
