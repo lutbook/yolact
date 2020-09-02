9ababf0015796f5ce576acd4f89dc78b187dd990
lutbook <lutbuk@gmail.com>
2020-09-02 18:01:41 +0900

    run change

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd external/DCNv2
+cd yolact/external/DCNv2
+pwd
 python setup.py build develop
-cd
-cd
-run python3 train.py --config=yolact_plus_custom_config --resume=/kw_resources/yolact_data/weights/yolact_resnet101_line_tree_266_20000.pth --start_iter=20001 
+cd /
+pwd
+cd yolact
+pwd
+python3 train.py --config=yolact_plus_custom_config --resume=/kw_resources/yolact_data/weights/yolact_resnet101_line_tree_266_20000.pth --start_iter=20001 
