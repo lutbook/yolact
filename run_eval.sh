2a1b6df8f69a82067999ea0356be8066cb6b702e
lutbook <lutbuk@gmail.com>
2020-09-03 21:18:08 +0900

    run_eval

--- run_eval.sh
+++ run_eval.sh
@@ -6,4 +6,5 @@ python3 eval.py --config=yolact_plus_custom_config \
 --trained_model==/kw_resources/yolact_data/weights/yolact_resnet101_line_tree_893_67000.pth \
 --score threshold=0.15 \
 --top_k=10 \
---images=/kw_resources/yolact_data/test_images/:/kw_resources/yolact_data/output_images/:
+--images=/kw_resources/yolact_data/test_images/:/kw_resources/yolact_data/output_images/: \
+--display
