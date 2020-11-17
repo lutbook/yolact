cf8100ac13613f88ad5f0821f46316c120dd8b97
lutbook <lutbuk@gmail.com>
2020-11-17 12:21:01 +0900

    eval

--- run_eval_video.sh
+++ run_eval_video.sh
@@ -5,6 +5,7 @@ cd /yolact
 python3 eval.py --config=yolact_plus_custom_config \
 --trained_model=/kw_resources/yolact_data/weights/yolact_resnet50_line_tree_2346_176000.pth \
 --score_threshold=0.0 \
---top_k=14 \
---images=kw_resources/yolact_data/test_images/:/kw_resources/yolact_data/output_images/ \
---benchmark
+--top_k=2 \
+--video_multiframe=4 \
+--display_fps \
+--video=kw_resources/yolact_data/test_videos/P8220345.mp4:/kw_resources/yolact_data/output_videos/P8220345.mp4
