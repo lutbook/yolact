e980b9daeafd5e5a1e3d43a0cc978a58fec7da6c
lutbook <lutbuk@gmail.com>
2020-11-17 12:54:52 +0900

    ev vid

--- run_eval_video.sh
+++ run_eval_video.sh
@@ -2,10 +2,10 @@
 cd external/DCNv2
 python3 setup.py build develop
 cd /yolact
-python3 eval.py --config=yolact_resnet50_custom_config \
+python3 eval.py \
 --trained_model=/kw_resources/yolact_data/weights/yolact_resnet50_line_tree_2346_176000.pth \
 --score_threshold=0.0 \
 --top_k=2 \
 --video_multiframe=4 \
 --display_fps \
---video=kw_resources/yolact_data/test_videos/P8220345.mp4:/kw_resources/yolact_data/output_videos/P8220345.mp4
+--video=/kw_resources/yolact_data/test_videos/P8220345.mp4:/kw_resources/yolact_data/output_videos/P8220345.mp4
