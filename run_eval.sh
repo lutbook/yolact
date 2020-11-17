84f3829a512bfb0bfb5ded90367bca8c2bd0b882
lutbook <lutbuk@gmail.com>
2020-11-17 12:49:54 +0900

    ev img

--- run_eval.sh
+++ run_eval.sh
@@ -6,5 +6,5 @@ python3 eval.py --config=yolact_resnet50_custom_config \
 --trained_model=/kw_resources/yolact_data/weights/yolact_resnet50_line_tree_2346_176000.pth \
 --score_threshold=0.0 \
 --top_k=14 \
---images=kw_resources/yolact_data/test_images/:/kw_resources/yolact_data/output_images/ \
+--images=kw_resources/yolact_data/test_images:/kw_resources/yolact_data/output_images \
 --benchmark
