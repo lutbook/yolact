32cbe0421dbfb4e38ac60925e24ecaf23fa2c710
lutbook <lutbuk@gmail.com>
2020-11-28 21:53:19 +0900

    ff

--- run_eval_video.sh
+++ run_eval_video.sh
@@ -4,5 +4,5 @@ python3 eval.py --config=yolact_resnet50_custom_config \
 --trained_model=/kw_resources/yolact_data/weights/yolact_resnet50_line_tree_8146_611000.pth \
 --score_threshold=0.01 \
 --top_k=15 \
---video_multiframe==4 \
+--video_multiframe=4 \
 --video=/kw_resources/yolact_data/test_videos/P8220345.mp4:/kw_resources/yolact_data/out_P8220345.mp4
