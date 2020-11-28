69351643fb366672a25c352b2ebde43d49c91a1e
lutbook <lutbuk@gmail.com>
2020-11-28 22:25:10 +0900

    dd

--- run_eval.sh
+++ run_eval.sh
@@ -3,7 +3,7 @@ cd external/DCNv2
 python3 setup.py build develop
 cd /yolact
 python3 eval.py --config=yolact_resnet50_custom_config \
---trained_model=/kw_resources/yolact_data/weights/yolact_resnet50_line_tree_2346_176000.pth \
+--trained_model=/kw_resources/yolact_data/weights/1116/yolact_resnet50_line_tree_2346_176000.pth \
 --score_threshold=0.0 \
 --top_k=14 \
 --images=/kw_resources/yolact_data/test_images/:/kw_resources/yolact_data/output_images/ \
