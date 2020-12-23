c80315ed96d6c7018b0a44567d80d38ff75ae9c4
lutbook <lutbuk@gmail.com>
2020-12-23 22:54:54 +0900

    rasf

--- run_50.sh
+++ run_50.sh
@@ -5,4 +5,5 @@ cd /yolact
 python3 train.py \
 --config=yolact_resnet50_custom_config \
 --resume=/kw_resources/yolact_data/weights/resnet50-19c8e357.pth \
---start_iter=0
+--start_iter=0 \
+--batch_size=5
