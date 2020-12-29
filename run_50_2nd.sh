804ec3cedd1c4fc0509e3249578fd8bed41b81a6
lutbook <lutbuk@gmail.com>
2020-12-29 22:06:14 +0900

    dfads

--- run_50_2nd.sh
+++ run_50_2nd.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd external/DCNv2
-python3 setup.py build develop
-cd /yolact
 python3 train.py \
 --config=yolact_r50_config \
 --resume=/kw_resources/yolact_data/weights/yolact_r50_1319_99000.pth \
