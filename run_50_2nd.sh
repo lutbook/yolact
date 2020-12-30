62df03843a31b6d9fdc39bffdc43ac9513688d24
lutbook <lutbuk@gmail.com>
2020-12-31 03:40:45 +0900

    fdasf

--- run_50_2nd.sh
+++ run_50_2nd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 train.py \
 --config=yolact_r50_config \
---resume=/kw_resources/yolact_data/weights/yolact_r50_1319_99000.pth \
+--resume=/kw_resources/yolact_data/weights/yolact_r50_2639_198000.pth \
 --start_iter=-1
