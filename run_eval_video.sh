183bbb6ecd0388614a41d871094eee5a14b919be
lutbook <lutbuk@gmail.com>
2021-01-31 20:02:31 +0900

    vid

--- run_eval_video.sh
+++ run_eval_video.sh
@@ -5,4 +5,4 @@ python3 eval.py --config=yolact_r50_config \
 --score_threshold=0.5 \
 --top_k=15 \
 --video_multiframe=4 \
---video=/kw_resources/yolact_data/test_videos/P8220345.mp4:/kw_resources/yolact_data/out_P8220345.mp4
+--video=/kw_resources/yolact_data/test_videos/P8220358.mp4:/kw_resources/yolact_data/P8220358.mp4
