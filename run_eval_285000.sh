32e423045c6cd5da503e26b46caef5a19c9394ad
lutbook <lutbuk@gmail.com>
2021-01-31 19:03:04 +0900

    dfa

--- run_eval_285000.sh
+++ run_eval_285000.sh
@@ -4,7 +4,7 @@ python3 setup.py build develop
 cd /yolact
 python3 eval.py --config=yolact_r50_config \
 --trained_model=/kw_resources/yolact_data/weights/yolact_r50_3799_285000.pth \
---score_threshold=0.0 \
+--score_threshold=0.5 \
 --top_k=14 \
 --images=/kw_resources/yolact_data/test_images/:/kw_resources/yolact_data/output_images/ \
 --benchmark \
--- run_eval_video.sh
+++ run_eval_video.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /yolact
-python3 eval.py --config=yolact_resnet50_custom_config \
---trained_model=/kw_resources/yolact_data/weights/1116/yolact_resnet50_line_tree_8146_611000.pth \
---score_threshold=0.01 \
+python3 eval.py --config=yolact_r50_config \
+--trained_model=/kw_resources/yolact_data/weights/yolact_r50_3799_285000.pth \
+--score_threshold=0.5 \
 --top_k=15 \
 --video_multiframe=4 \
 --video=/kw_resources/yolact_data/test_videos/P8220345.mp4:/kw_resources/yolact_data/out_P8220345.mp4
