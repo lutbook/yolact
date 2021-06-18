c7a4472f5636bcc07533479788b606c2c614a62e
lutbook <lutbuk@gmail.com>
2021-06-18 18:25:41 +0900

    change 4 custom2 dataset

---
+++ run_50_custom2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd external/DCNv2
+python3 setup.py build develop
+cd /yolact
+python3 train.py \
+--config=yolact_r50_custom2_config \
+--resume=/kw_resources/yolact_data/weights/resnet50-19c8e357.pth \
+--start_iter=0
